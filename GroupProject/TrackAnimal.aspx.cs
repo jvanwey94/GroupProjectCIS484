294806169060ad33783544a0c3a2b7eed9b20d2b
Lingjing Jiang <jianglx@dukes.jmu.edu>
2018-11-12 21:56:56 -0500

    table

--- GroupProject/TrackAnimal.aspx.cs
+++ GroupProject/TrackAnimal.aspx.cs
@@ -11,4 +11,29 @@ public partial class TrackAnimal : System.Web.UI.Page
     {
 
     }
+    protected override void Render(HtmlTextWriter writer)
+    {
+        foreach (GridViewRow r in gvRegularProgram.Rows)
+        {
+            if (r.RowType == DataControlRowType.DataRow)
+            {
+                r.Attributes["onmouseover"] = "this.style.cursor='pointer';this.style.textDecoration='underline';";
+                r.Attributes["onmouseout"] = "this.style.textDecoration='none';";
+                r.ToolTip = "Click to select row";
+                r.Cells[1].Attributes["onclick"] = this.Page.ClientScript.GetPostBackClientHyperlink(this.gvRegularProgram, "Select$" + r.RowIndex, true);
+
+            }
+        }
+
+        base.Render(writer);
+    }
+    protected void OnSelectedIndexChanged(object sender, EventArgs e)
+    {
+
+        Page.ClientScript.RegisterStartupScript(this.GetType(), "CallMyFunction", "codeAddress()", true);
+
+        
+       
+    }
+
 }
